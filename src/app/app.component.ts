f52bbc74e7e4ca9671ce546bcf230adaad939f93
Andreas W. <bahamut13+github0@gmail.com>
2025-03-03 18:37:18 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LandingPageComponent } from './landingPage/landingPage.component'
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, LandingPageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'sakura';
+}
