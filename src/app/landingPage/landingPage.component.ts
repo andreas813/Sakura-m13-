a29a654d3de756a04446feb632f7484e77844e83
Andreas W. <bahamut13+github0@gmail.com>
2025-03-04 23:26:49 +0100

    added image assets

--- src/app/landingPage/landingPage.component.ts
+++ src/app/landingPage/landingPage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
     selector: 'app-landingPage',
     standalone: true,
-    imports: [],
-    template: `
+    imports: [NavbarComponent],
+    template: /*html*/`
     <section>
+        <app-navbar></app-navbar>
         <h1 class="fontRaleway">SAKURA RAMEN</h1>
         <h2 class="fontRaleway">BEST RAMEN IN TOWN</h2>
     </section>
