e5a88aabac48b4eee10f9d9b6de6a46fc945ce9c
Andreas W. <bahamut13+github0@gmail.com>
2025-03-08 22:13:50 +0100

    set up routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from "./main-content/main-content.component";
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MainContentComponent],
+  imports: [RouterOutlet, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/main-content/landing-page/landing-page.component.ts
+++ src/app/main-content/landing-page/landing-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
     selector: 'app-landingPage',
     standalone: true,
+    imports: [NavbarComponent],
     templateUrl: './landing-page.component.html',
     styleUrl: './landing-page.components.scss',
 })
